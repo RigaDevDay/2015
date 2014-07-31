b37485278fa135badace672cf5f13077160582c8
Alexey Buzdin <alex.buzdin@gmail.com>
2014-07-31 23:58:11 +0300

    Fixed typo in deadline

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('#countdown').flipcountdown({
 			size:'lg',
-			beforeDateTime:'15/07/2014 00:00:01',
+			beforeDateTime:'15/09/2014 00:00:01',
 			speedFlip:60
 			/*tick:function(){
 				var nol = function(h){
