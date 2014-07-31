3f6c2ea91695403ebcb552b025637dad48112a34
Dmitry Papka <dmitry-papka@github.com>
2014-07-31 21:44:23 +0300

    Replacing old countdown script with the new one

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-	// Grab the current date
-	var currentDate = new Date();
-
-	// Set some date in the future. In this case, it's always Jan 1
-	var futureDate  = new Date(2014, 12, 1);
-
-	// Calculate the difference in seconds between the future and current date
-	var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-
-	// Instantiate a coutdown FlipClock
-	clock = $('#countdown').FlipClock(diff, {
-		clockFace: 'DailyCounter',
-		countdown: true,
-		showSeconds: true
-
-	});
+	$('#countdown').flipcountdown({
+			size:'lg',
+			beforeDateTime:'1/01/2015 00:00:01',
+			speedFlip:60
+			/*tick:function(){
+				var nol = function(h){
+					return h>9?h:'0'+h;
+				}
+				var	range  	= NY-Math.round((new Date()).getTime()/1000),
+					secday = 86400, sechour = 3600,
+					days 	= parseInt(range/secday),
+					hours	= parseInt((range%secday)/sechour),
+					min		= parseInt(((range%secday)%sechour)/60),
+					sec		= ((range%secday)%sechour)%60;
+				return nol(days)+' '+nol(hours)+' '+nol(min)+' '+nol(sec);
+			}*/
+		});
 	$(".header").positionSticky();
 	$(".header").css("width", "100%");
 });
